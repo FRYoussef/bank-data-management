2af5466b5f66f663446466f8361a4932326f136c
FRYoussef <f.r.youssef@hotmail.com>
2019-12-25 20:21:35 +0100

    Added first version of hive script

---
+++ data-warehouse/hive-create-schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS bankDataWarehouse;
+
+CREATE TABLE IF NOT EXISTS factSells (
+    productType: STRING,
+    productCategory: STRING,
+    productInvestment: DOUBLE,
+    currency: STRING,
+    interestType: STRING,
+    interest: FLOAT,
+    sellDate: TIMESTAMP,
+    customerId: INT,
+    customerBirthDate: TIMESTAMP,
+    customerPostCode: INT,
+    customerCity: STRING,
+    customerRegion: STRING,
+    customerCountry: STRING,
+    customerSalary: DOUBLE,
+    customerGender: STRING,
+    customerUnitsSold: INT,
+    customerTotalRevenue: DOUBLE,
+    customerType: STRING)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n';
+
+
+
+DESCRIBE factSells;
