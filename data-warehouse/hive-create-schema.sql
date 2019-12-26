3dfad5ab2c2833c33d6e2f1459a3ff80fdc5bec6
FRYoussef <f.r.youssef@hotmail.com>
2019-12-26 14:58:17 +0100

    Added .gitignore

--- data-warehouse/hive-create-schema.sql
+++ data-warehouse/hive-create-schema.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE IF NOT EXISTS bankDataWarehouse;
 
 CREATE TABLE IF NOT EXISTS factSells (
-    productType: STRING,
-    productCategory: STRING,
-    productInvestment: DOUBLE,
-    currency: STRING,
-    interestType: STRING,
-    interest: FLOAT,
-    sellDate: TIMESTAMP,
-    customerId: INT,
-    customerBirthDate: TIMESTAMP,
-    customerPostCode: INT,
-    customerCity: STRING,
-    customerRegion: STRING,
-    customerCountry: STRING,
-    customerSalary: DOUBLE,
-    customerGender: STRING,
-    customerUnitsSold: INT,
-    customerTotalRevenue: DOUBLE,
-    customerType: STRING)
+    productType STRING,
+    productCategory STRING,
+    productInvestment DOUBLE,
+    currency STRING,
+    interestType STRING,
+    interest FLOAT,
+    sellDate TIMESTAMP,
+    customerId INT,
+    customerBirthDate TIMESTAMP,
+    customerPostCode INT,
+    customerCity STRING,
+    customerRegion STRING,
+    customerCountry STRING,
+    customerSalary DOUBLE,
+    customerGender STRING,
+    customerUnitsSold INT,
+    customerTotalRevenue DOUBLE,
+    customerType STRING)
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n';
