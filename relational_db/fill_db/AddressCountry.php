7394f5986a8602ba09c1f0e2bc97a99407905e76
Raquel Blanco <raqblanc@MacBook-Air-de-Raquel.local>
2019-12-29 19:28:16 +0100

    Credit/Debit Card

---
+++ relational_db/fill_db/AddressCountry.php
@@ -0,0 +1,31 @@
+
+<?php 
+$servername = "localhost"; 
+$username = "root"; 
+$password = "";
+$dbname = "Bank-data-management"; 
+  
+// Create connection 
+$conn = mysqli_connect( $servername, $username, $password, $dbname ); 
+  
+// Check connection 
+if ( !$conn ) { 
+    die("Connection failed: " . mysqli_connect_error()); 
+} 
+
+
+$sql="INSERT INTO `AddressCountry` VALUES ('1','Spain',now()),
+('2','USA', now()),
+('3','China', now()),
+('4','France',now());";
+
+
+if (mysqli_query($conn, $sql)) { 
+    echo "New record created successfully"; 
+} else { 
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn); 
+}
+
+// Close coneection 
+mysqli_close($conn); 
+?>
---
+++ relational_db/fill_db/AddressCountry.sql
@@ -0,0 +1,4 @@
+INSERT INTO `AddressCountry` VALUES ('1','Spain',now()),
+('2','USA', now()),
+('3','China', now()),
+('4','France',now()); 
